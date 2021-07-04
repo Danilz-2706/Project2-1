6f10cbd03dfc862104e498c23a8a03e0592d866c
Admin <Admin@ADMIN>
2021-07-04 14:10:13 +0700

    Add project files.

---
+++ Project2-1.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Project2_1.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ Project2-1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project2_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
